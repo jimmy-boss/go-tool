3e5a207432ddf979db8d7677bd3b39f4458896b4
Jimmy <jimmyhuangboss@gmail.com>
2025-11-20 10:14:25 +0800

    Extended usage of the go function 2 and remove fast func Go

--- gwaitgroup/func.go
+++ gwaitgroup/func.go
@@ -8,13 +8,86 @@
 // --------------------------------------------
 package gwaitgroup
 
-import "sync"
+// GoInt Extended usage of the "go" function, with parameter "Content" and "Int"
+func (wg *WaitGroup) GoInt(f func(index int), index int) {
+	wg.Go(func() {
+		f(index)
+	})
+}
 
-// Go is a wrapper for sync.WaitGroup, providing a more convenient way to use sync.WaitGroup.Add and sync.WaitGroup.Done.
-func Go(wg *sync.WaitGroup, f func()) {
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		f()
-	}()
+// GoInt64 Extended usage of the "go" function, with parameter "Content" and "Int64"
+func (wg *WaitGroup) GoInt64(f func(index int64), index int64) {
+	wg.Go(func() {
+		f(index)
+	})
+}
+
+// GoInt32 Extended usage of the "go" function, with parameter "Content" and "Int32"
+func (wg *WaitGroup) GoInt32(f func(index int32), index int32) {
+	wg.Go(func() {
+		f(index)
+	})
+}
+
+// GoInt16 Extended usage of the "go" function, with parameter "Content" and "Int16"
+func (wg *WaitGroup) GoInt16(f func(index int16), index int16) {
+	wg.Go(func() {
+		f(index)
+	})
+}
+
+// GoInt8 Extended usage of the "go" function, with parameter "Content" and "Int8"
+func (wg *WaitGroup) GoInt8(f func(index int8), index int8) {
+	wg.Go(func() {
+		f(index)
+	})
+}
+
+// GoUInt Extended usage of the "go" function, with parameter "Content" and "UInt"
+func (wg *WaitGroup) GoUInt(f func(index uint), index uint) {
+	wg.Go(func() {
+		f(index)
+	})
+}
+
+// GoUInt64 Extended usage of the "go" function, with parameter "Content" and "UInt64"
+func (wg *WaitGroup) GoUInt64(f func(index uint64), index uint64) {
+	wg.Go(func() {
+		f(index)
+	})
+}
+
+// GoUInt32 Extended usage of the "go" function, with parameter "Content" and "UInt32"
+func (wg *WaitGroup) GoUInt32(f func(index uint32), index uint32) {
+	wg.Go(func() {
+		f(index)
+	})
+}
+
+// GoUInt16 Extended usage of the "go" function, with parameter "Content" and "UInt16"
+func (wg *WaitGroup) GoUInt16(f func(index uint16), index uint16) {
+	wg.Go(func() {
+		f(index)
+	})
+}
+
+// GoUInt8 Extended usage of the "go" function, with parameter "Content" and "UInt8"
+func (wg *WaitGroup) GoUInt8(f func(index uint8), index uint8) {
+	wg.Go(func() {
+		f(index)
+	})
+}
+
+// GoString Extended usage of the "go" function, with parameter "Content" and "String"
+func (wg *WaitGroup) GoString(f func(str string), str string) {
+	wg.Go(func() {
+		f(str)
+	})
+}
+
+// GoBytes Extended usage of the "go" function, with parameter "Content" and "Bytes"
+func (wg *WaitGroup) GoBytes(f func(byteSlice []byte), byteSlice []byte) {
+	wg.Go(func() {
+		f(byteSlice)
+	})
 }
